ad55fbfb9a560528f5dedce04245e9ed516f7d2f
Jeremy Simpson <jsimpson@med.unc.edu>
2017-07-08 14:11:59 -0400

    updated ajax calls and on-click button for add food

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,33 @@
-var applicationIDNutrition = "570420e6";
-var apiKeyNutrition = "3f9b7cc73ed56deaca89dd781453c5e7";
-var queryURLNutrition = "https://api.nutritionix.com/v1_1/search/cheeseburger?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId="+ applicationIDNutrition + "&appKey=" + apiKeyNutrition + "";
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC3VxUOsWXYcjoYzefgW9bW1WJ8pJH6BQ4",
+    authDomain: "mealmaker-a5669.firebaseapp.com",
+    databaseURL: "https://mealmaker-a5669.firebaseio.com",
+    projectId: "mealmaker-a5669",
+    storageBucket: "",
+    messagingSenderId: "158039007710"
+  };
 
-var apiKeyImage = "key=5847309-0a6ea96477b972ed6e131f630&";
-var queryURLImage = "https://pixabay.com/api/?" + apiKeyImage + "q=cheeseburger" + "&image_type=photo";
+ var webformatURL;
 
+$(document).ready(function () {
+  console.log("ready");
+});
+
+$("#food-input-button").click(function(e) { 
+  e.preventDefault();
+  createFoodImage();
+  var foodInput = $("#food-input").val().trim();
+  console.log(foodInput);
+  var applicationIDNutrition = "570420e6";
+  var apiKeyNutrition = "3f9b7cc73ed56deaca89dd781453c5e7";
+  var queryURLNutrition = "https://api.nutritionix.com/v1_1/search/" + foodInput + "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId="+ applicationIDNutrition + "&appKey=" + apiKeyNutrition;
+
+  var apiKeyImage = "key=5847309-0a6ea96477b972ed6e131f630&q=";
+  var queryURLImage = "https://pixabay.com/api/?" + apiKeyImage + foodInput + "&image_type=photo";
+
+
+  
 $.ajax ({
     url: (queryURLNutrition),
     method: "GET"
@@ -13,25 +36,35 @@ $.ajax ({
 });
 
 $.ajax ({
+    
     url: (queryURLImage),
     method: "GET"
 }).done(function(imageResponse){
-  console.log(imageResponse);
+  webformatURL = (imageResponse.hits[1].webformatURL);
 });
 
+});
 
-// https://pixabay.com/api/?key=5847309-0a6ea96477b972ed6e131f630&q=yellow+flowers&image_type=photo
+function createFoodImage(){
+  var newDiv = $("<div>");
+  newDiv.addClass = $("food-image");
+  var foodImage = $("<img src=" + webformatURL);
+  newDiv.append(foodImage);
+  $(".panel-body").append(newDiv);
+};
 
-// // Initialize Firebase
-//   var config = {
-//     apiKey: "AIzaSyBKpIFAQruvPekpkeqncOcIPpnnK78U6Zg",
-//     authDomain: "mealmaker-3f984.firebaseapp.com",
-//     databaseURL: "https://mealmaker-3f984.firebaseio.com",
-//     projectId: "mealmaker-3f984",
-//     storageBucket: "mealmaker-3f984.appspot.com",
-//     messagingSenderId: "776198822347"
-//   };
-//   firebase.initializeApp(config);
 
-// console.log("test");
+
+// // // Initialize Firebase
+// //   var config = {
+// //     apiKey: "AIzaSyBKpIFAQruvPekpkeqncOcIPpnnK78U6Zg",
+// //     authDomain: "mealmaker-3f984.firebaseapp.com",
+// //     databaseURL: "https://mealmaker-3f984.firebaseio.com",
+// //     projectId: "mealmaker-3f984",
+// //     storageBucket: "mealmaker-3f984.appspot.com",
+// //     messagingSenderId: "776198822347"
+// //   };
+// //   firebase.initializeApp(config);
+
+// // console.log("test");
 
