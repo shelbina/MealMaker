f5b06f90d3b045d49a1a588251c190d729e70257
Jeremy Simpson <jsimpson@med.unc.edu>
2017-07-09 11:05:50 -0400

    Images being Added Successfully
    Add Food Successfully adds image in Images Box on the Right hand side

--- assets/script.js
+++ assets/script.js
@@ -1,73 +1,73 @@
  var config = {
-    apiKey: "AIzaSyBKpIFAQruvPekpkeqncOcIPpnnK78U6Zg",
-    authDomain: "mealmaker-3f984.firebaseapp.com",
-    databaseURL: "https://mealmaker-3f984.firebaseio.com",
-    projectId: "mealmaker-3f984",
-    storageBucket: "mealmaker-3f984.appspot.com",
-    messagingSenderId: "776198822347"
-  };
+   apiKey: "AIzaSyBKpIFAQruvPekpkeqncOcIPpnnK78U6Zg",
+   authDomain: "mealmaker-3f984.firebaseapp.com",
+   databaseURL: "https://mealmaker-3f984.firebaseio.com",
+   projectId: "mealmaker-3f984",
+   storageBucket: "mealmaker-3f984.appspot.com",
+   messagingSenderId: "776198822347"
+ };
 
-  firebase.initializeApp(config);
-  
-  var database = firebase.database();
+ firebase.initializeApp(config);
 
- var webformatURL;
+ var database = firebase.database();
 
-$(document).ready(function () {
-  console.log("ready");
-});
+ $(document).ready(function () {
+   console.log("ready");
+ });
 
-$("#food-input-button").click(function(e) { 
-  e.preventDefault();
-  createFoodImage();
-  var foodInput = $("#food-input").val().trim();
-  console.log(foodInput);
-  var applicationIDNutrition = "570420e6";
-  var apiKeyNutrition = "3f9b7cc73ed56deaca89dd781453c5e7";
-  var queryURLNutrition = "https://api.nutritionix.com/v1_1/search/" + foodInput + "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId="+ applicationIDNutrition + "&appKey=" + apiKeyNutrition;
+ $("#food-input-button").click(function (e) {
+   e.preventDefault();
+   var foodInput = $("#food-input").val().trim();
+   console.log(foodInput);
+   var applicationIDNutrition = "570420e6";
+   var apiKeyNutrition = "3f9b7cc73ed56deaca89dd781453c5e7";
+   var queryURLNutrition = "https://api.nutritionix.com/v1_1/search/" + foodInput +
+     "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=" +
+     applicationIDNutrition + "&appKey=" + apiKeyNutrition;
 
-  var apiKeyImage = "key=5847309-0a6ea96477b972ed6e131f630&q=";
-  var queryURLImage = "https://pixabay.com/api/?" + apiKeyImage + foodInput + "&image_type=photo";
+   var apiKeyImage = "key=5847309-0a6ea96477b972ed6e131f630&q=";
+   var queryURLImage = "https://pixabay.com/api/?" + apiKeyImage + foodInput + "&image_type=photo";
 
 
-  
-$.ajax ({
-    url: (queryURLNutrition),
-    method: "GET"
-}).done(function(nutritionResponse){
-  console.log(nutritionResponse);
-});
 
-$.ajax ({
-    
-    url: (queryURLImage),
-    method: "GET"
-}).done(function(imageResponse){
-  webformatURL = (imageResponse.hits[0].webformatURL);
-  console.log("Click this " + webformatURL);
-});
+   $.ajax({
+     url: (queryURLNutrition),
+     method: "GET"
+   }).done(function (nutritionResponse) {
+     console.log(nutritionResponse);
+   });
 
-});
+   $.ajax({
 
-function createFoodImage(){
- // var newDiv = $("<div>");
- // newDiv.addClass = $("food-image");
-  var foodImage = $("<img height=200px width=200px src="+ webformatURL +">");
-  // newDiv.append(foodImage);
-  $(".panel-body").append(foodImage);
-};
+     url: (queryURLImage),
+     method: "GET"
+   }).done(function (imageResponse) {
+     webformatURL = (imageResponse.hits[0].webformatURL);
+     console.log("Click this " + webformatURL);
+     createFoodImage();
+   });
 
-console.log("test");
+ });
 
-// var animate = response.data[i].images.downsized.url;
-//     var newDiv =$("<div>");
-//     newDiv.addClass("newDiv");
-//     var rating = $("<p>").text("Rating: " + response.data[i].rating);
-//     newDiv.append(rating);
-//     var image = $("<img height=200px width=200px>");
-//    image.attr("data-state", "still");
-//    image.attr("data-still", still);
-//    image.attr("data-animate", animate);
-//    image.attr("src", still);
-//     newDiv.append(image);
-//     $("#gifImages").prepend(newDiv);
+ function createFoodImage() {
+   // var newDiv = $("<div>");
+   // newDiv.addClass = $("food-image");
+   var foodImage = $("<img height=200px width=200px src=" + webformatURL + ">");
+   // newDiv.append(foodImage);
+   $(".panel-body").append(foodImage);
+ }
+
+ console.log("test");
+
+ // var animate = response.data[i].images.downsized.url;
+ //     var newDiv =$("<div>");
+ //     newDiv.addClass("newDiv");
+ //     var rating = $("<p>").text("Rating: " + response.data[i].rating);
+ //     newDiv.append(rating);
+ //     var image = $("<img height=200px width=200px>");
+ //    image.attr("data-state", "still");
+ //    image.attr("data-still", still);
+ //    image.attr("data-animate", animate);
+ //    image.attr("src", still);
+ //     newDiv.append(image);
+ //     $("#gifImages").prepend(newDiv);
