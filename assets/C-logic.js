268bab0c027267698e851cde9610d8a109226fcb
Will H <wj_hilliard@yahoo.com>
2017-07-11 20:20:27 -0400

    hopefully just images, html, and css

--- assets/C-logic.js
+++ assets/C-logic.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Activate Carousel
-    $("#myCarousel").carousel({interval: 1000});
+    $("#myCarousel").carousel({interval: 3000});
     
     // Enable Carousel Indicators
     $(".item1").click(function(){
